bd66894b8c5191c8179045cc62cc792047220c83
Erfan_k <95238257+Erfk@users.noreply.github.com>
2023-07-21 02:59:00 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xf27d8bbc160b032f5d261ee2ceea302aa16fbca9";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
